d3c25b2c4e5d176352dd83d951374e5b69aba59d
Piette Quentin <q.piette@rubika-edu.com>
2024-10-08 12:54:22 +0200

    Ball script

--- ConsoleApplication1/un jeu/Ball.cs
+++ ConsoleApplication1/un jeu/Ball.cs
@@ -1,7 +1,70 @@
-﻿namespace un_jeu
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace un_jeu
 {
-    public class Ball
+    class Ball
     {
+        public int X { set; get; }
+        public int Y { set; get; }
+        int zwrotX;
+        int zwrotY;
+        int i;
+        int boardHeight;
+        int boardWidht;
+        
+        public int direction {set;get;}
+        public Ball (int x, int y)
+        {
+            X = x;
+            Y = y;
+            this.boardHeight = boardHeight;
+            this.boardWidht = boardWidht;
+            zwrotX = -1;
+            zwrotY = 1;
+        }
+
+        public void Logic(Paddle paddle1, Paddle paddle2)
+        {
+            Console.SetCursorPosition(X,Y);
+            Console.Write("\0");
+            if (Y <= 1 || Y >= boardHeight)
+            {
+                zwrotY *= -1;
+            }
+
+            if (((X == 3 || X == boardWidht - 3) && (paddle1.Y - (paddle1.Lenght / 2)) <= Y && (paddle1.Y + (paddle1.Lenght / 2)) > Y))
+            {
+                zwrotX *= 1;
+                if (Y == paddle1.Y)
+                {
+                    direction = 0;
+                }
+
+                if (Y >= paddle1.Y - paddle1.Lenght/2)
+                {
+                    direction = 1;
+                }
+
+                switch (direction)
+                {
+                   case 0:
+                       X += zwrotX;
+                       Y += zwrotY;
+                       break;
+                }
+                
+            }
+        }
+
+        public void Write()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(X,Y);
+            Console.Write("■");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
         
     }
+   
 }
